cb033bcb29099ea23dc0a9906b57915f33feadf8
Aryan Sahu <aryansahu.vibranium@gmail.com>
2023-11-18 22:54:49 +0530

    program 2

---
+++ pythonassignments/program2.py
@@ -0,0 +1,10 @@
+total_card = 52
+kings = 4
+spade = 13
+
+neither_king_nor_spade = total_card -kings-spade+1
+
+probability = neither_king_nor_spade/total_card
+
+print("Probability of neither king nor spade",probability)
+
