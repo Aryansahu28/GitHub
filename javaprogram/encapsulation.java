2b70f0dc35e9d7d1d51fbd3a5db28633912488d8
Aryan Sahu <aryansahu.vibranium@gmail.com>
2024-02-29 22:52:58 +0530

    Java for sessional

---
+++ javaprogram/encapsulation.java
@@ -0,0 +1,27 @@
+class Person{
+    private int age;
+    private String name;
+    public void setage(int a){
+        age = a;
+    }
+    public void setname(String n){
+        name = n;
+    }
+    public int getage(){
+        return age;
+    }
+    public String getname(){
+        return name;
+    }
+}
+
+public class encapsulation {
+    public static void main(String args[]){
+        Person p1 = new Person();
+        p1.setage(12);
+        p1.setname("Ravi");
+        System.out.println(p1.getage());
+        System.out.println(p1.getname());
+
+    }
+}
