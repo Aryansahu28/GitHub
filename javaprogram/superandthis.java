427a270acde1065010a31a96333670afa78958d3
Aryan Sahu <aryansahu.vibranium@gmail.com>
2024-03-07 15:54:30 +0530

    Super and this keyword

---
+++ javaprogram/superandthis.java
@@ -0,0 +1,27 @@
+class A{
+    A(){
+        
+        super();
+        System.out.println("in A");
+    }
+    A(int n){
+        super();
+        System.out.println("in A int");
+    }
+}
+class B extends A{
+    B(){
+        super();
+        System.out.println("in B");
+    }
+    B(int n){
+        super(5);
+        System.out.println("in B int");
+    }
+}
+public class superandthis {
+    public static void main(String[] args){
+        B obj = new B(5);
+    }
+    
+}
