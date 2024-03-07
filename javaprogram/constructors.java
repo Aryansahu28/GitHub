5a5357e2afda948d0cce7891434d2c72bff74907
Aryan Sahu <aryansahu.vibranium@gmail.com>
2024-03-07 12:49:43 +0530

    Constructors

---
+++ javaprogram/constructors.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Animal{
+    Animal(){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        System.out.println("Your pet name is " + name);
+
+    }
+}
+
+public class constructors {
+    public static void main(String[] args) {
+        Animal an = new Animal();
+    }
+}
