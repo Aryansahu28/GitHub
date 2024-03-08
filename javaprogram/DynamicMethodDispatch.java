15ee7aed7c05e510011054a16a42af29f1f0cec8
Aryan Sahu <aryansahu.vibranium@gmail.com>
2024-03-08 22:29:17 +0530

    Java 2

---
+++ javaprogram/DynamicMethodDispatch.java
@@ -0,0 +1,24 @@
+// Dynamic Method Dispatch in java describes about how we can initiate the objects in the program and what will be the outcome if we use Different class name and inherited class name what will be the outcome of it. It is another type of Polymorphism.
+class A{
+    public void show(){
+        System.out.println("In A show");
+    }
+}
+
+class B extends A{
+    public void show(){
+        System.out.println("In B show");
+    }
+}
+
+public class DynamicMethodDispatch {
+    public static void main(String args[]){
+        
+        A obj = new A();
+        obj.show();
+
+        obj = new B();
+        obj.show();
+    }
+}
+
