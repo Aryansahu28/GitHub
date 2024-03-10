3e555ee4adb2388a779b62626f7aa1a30d22c7e1
Aryan Sahu <aryansahu.vibranium@gmail.com>
2024-03-10 15:01:37 +0530

    dsf

---
+++ javaassignment/vofcylin.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class vofcylin {
+    public static void main(String[] args){
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter height of Cylinder");
+            //height of cylinder
+            int h = sc.nextInt();
+            System.out.println("Enter radius of Cylinder");
+            int r = sc.nextInt();
+            double V = (double)3.14*r*r*h;
+            System.out.print("Volume of Cylinder is "+ V);
+        }
+    }
+}
