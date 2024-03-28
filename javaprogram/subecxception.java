ef5c53e1a56c25420abbf11b3fc0c93982b58a95
Aryan Sahu <aryansahu.vibranium@gmail.com>
2024-03-28 18:58:11 +0530

    Unreachable subclass

---
+++ javaprogram/subecxception.java
@@ -0,0 +1,16 @@
+public class subecxception {
+    public static void main(String args[]){
+        try{
+            int a = 40;
+            int b =0;
+            int c = a/b;
+            System.out.println(c);
+        }
+        catch(Exception e){
+            System.out.println("Exception :" + e);
+        }
+        // catch(ArithmeticException f){
+
+        // }
+    }   
+}
