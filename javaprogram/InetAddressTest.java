019c8a9ab23958e6e4a5e56f227ba542f435da35
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-04-12 14:16:50 +0530

    InetAddress

---
+++ javaprogram/InetAddressTest.java
@@ -0,0 +1,15 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InetAddressTest {
+    public static void main(String args[]) throws UnknownHostException{
+        InetAddress Address = InetAddress.getLocalHost();
+        System.out.println(Address);
+        Address = InetAddress.getByName("www.google.com");
+        System.out.println(Address);
+        InetAddress sw[] =InetAddress.getAllByName("www.google.com");
+        for(int i=0;i< sw.length;i++){
+            System.out.println(sw[i]);
+        }
+    }
+}
