bd466ebaaa29263f4f979bcfe79c825df75db429
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-04-19 13:01:24 +0530

    Assignment-1

---
+++ javaassignment/ass_a_10.java
@@ -0,0 +1,9 @@
+public class ass_a_10 {
+   public static void main(String args[]){
+    int a =5;
+    int b=3;
+    int c = 7;
+    int larger = (a>b)?(a>c?a:c):(b>c?b:c);
+    System.out.println(larger);
+   } 
+}
