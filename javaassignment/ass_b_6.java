944cd3128f3877ef1fe9f7b4ccf5bebff0ebd51c
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-04-21 09:39:48 +0530

    Assignment-2

---
+++ javaassignment/ass_b_6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ass_b_6 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for (int i =1;i<=x;i++){
+            System.out.println(i*x);
+        }
+    }
+}
