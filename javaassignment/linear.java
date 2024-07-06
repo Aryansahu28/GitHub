59e23c57519fb51a783d9300968480b3331d3682
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-07-06 16:46:06 +0530

    lin

---
+++ javaassignment/linear.java
@@ -0,0 +1,15 @@
+public class linear{
+    public static void main(String[] args) {
+        String name = "kunal";
+        char target = 'r';
+        System.out.println(linearSearch(name,target));
+
+    }
+    static boolean linearSearch(String n,char target){
+        for(int i =0;i<n.length();i++){
+            if(target == n.charAt(i))
+            return true;
+        }
+        return false;
+    }
+}
