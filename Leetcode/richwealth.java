d642e16243faf6ae8d1318660a7802d174dea46c
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-07-09 17:28:36 +0530

    richwealth

--- Leetcode/richwealth.java
+++ Leetcode/richwealth.java
@@ -2,5 +2,28 @@
 
 // A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
 public class richwealth {
+    public static void main(String[] args){
+        int[][] arr = {{2,8,7},{7,1,3},{1,9,5}};
+        System.out.println(maximumWealth(arr));
+
+    }
+    static int maximumWealth(int[][] accounts){
+        int max=0;
+        int max_check=0;
+        
+        for(int[] a: accounts){
+            for(int r : a){
+                max_check = max_check + r;
+            }
+            if(max_check > max){
+                max = max_check;
+            }
+            max_check=0;
+        }
+        return max;
+    }
+
     
+
 }
+
