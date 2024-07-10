23e3bde67a1494008cd8c2b558c77795a112c2d5
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-07-10 15:39:13 +0530

    javascript

---
+++ Learning_javascript/script_01.js
@@ -0,0 +1,14 @@
+var a = 6;
+console.log(a);
+let b = 4;
+let c = "Hae";
+const d = 45;
+let x = null;
+let y = undefined;
+console.log(b, c, d, x, y);
+console.log(typeof b, typeof c, typeof d, typeof x, typeof y);
+let o = {
+    "name": "Aryan",
+    "job_num": 45
+}
+console.log(o);
