17efb66f3935ade057268eac8ec139633a63a2b9
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-07-16 19:16:55 +0530

    f

---
+++ Learning_javascript/functions.js
@@ -0,0 +1,8 @@
+function call(name) {
+    console.log("Hello " + name + ", I think you're fine just the way you looking you are loooking....");
+}
+
+
+call("Ravi")
+call("Ahiravan")
+call("Shivam")
