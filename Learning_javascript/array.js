4e0cfe638d23177fa2f8cb0df0cb3d1427842cdc
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-07-20 20:30:20 +0530

    f

--- Learning_javascript/array.js
+++ Learning_javascript/array.js
@@ -3,4 +3,15 @@ let arr = [1, 2, 3, 4, 5];
 let newarr = arr.map((e) => {
     return e ** 2;
 })
-console.log(newarr);
+
+
+const greaterthanseven = ((e) => {
+    if (e > 2) {
+        return true;
+    }
+    return false;
+})
+
+let newArr = arr.filter(greaterthanseven);
+
+console.log(newArr);
