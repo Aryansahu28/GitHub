45d706ba7042e8ce2fa108ced9f50b48dfdff1a0
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-07-25 21:44:21 +0530

    s

--- javaassignment/builderforstring.java
+++ javaassignment/builderforstring.java
@@ -1,11 +1,20 @@
 public class builderforstring {
 
     public static void main(String[] args) {
+        // Without using StringBuilder
         String series = "";
         for(int i=0;i<26;i++){
             char ch = (char)('a' + i);
             series = series + ch;
         }
         System.out.println(series);
+
+        // With using StringBuilder
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<26;i++){
+            char ch = (char)('a' + i);
+            sb.append(ch);
+        }
+        System.out.println(sb);
     }
 }
