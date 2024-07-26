37a31db86687ef63e8f9529c2e6b18159c124c6f
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-07-26 21:36:26 +0530

    a

---
+++ Learning_javascript/exercise12.js
@@ -0,0 +1,10 @@
+document.getElementById("box1").style.backgroundColor = "red";
+document.getElementById("box2").style.color = "red";
+document.getElementById("box2").style.backgroundColor = "blue";
+document.getElementById("box3").style.color = "blue";
+document.getElementById("box3").style.backgroundColor = "Grey";
+document.getElementById("box4").style.color = "Grey";
+document.getElementById("box4").style.backgroundColor = "Yellow";
+document.getElementById("box5").style.color = "Yellow";
+document.getElementById("box5").style.backgroundColor = "Orange";
+document.getElementById("box1").style.color = "Orange";
