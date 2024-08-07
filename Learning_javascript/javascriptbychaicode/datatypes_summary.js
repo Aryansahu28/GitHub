6b67151c4b804ee2106008f01c648f821286e74f
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-08-07 18:58:12 +0530

    datatypes

---
+++ Learning_javascript/javascriptbychaicode/datatypes_summary.js
@@ -0,0 +1,20 @@
+
+// Reference (Non-primitive);
+//     ||
+//     \/ 
+// Arrays,Object,functions
+
+
+
+const heros = ["shaktiman" , "Spiderman", "SuryakumarYadav"]
+
+let myObj = {
+    name : "hitesh",
+    age : 22
+}
+
+const myFunction = function(){
+    console.log("Hello World!")
+}
+
+myFunction()
