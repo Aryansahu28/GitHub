d49e0cc426db3c412be0b44a3434d5f8eb609536
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-08-10 19:21:42 +0530

    array_02

--- Learning_javascript/javascriptbychaicode/array_02.js
+++ Learning_javascript/javascriptbychaicode/array_02.js
@@ -16,4 +16,13 @@ const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const real_another_array = another_array.flat(Infinity);
 // console.log(real_another_array)
 
-console.log(Array.isArray("IronMan"))
+console.log(Array.isArray(["IronMan"]))
+console.log(Array.from("Hitesh"))
+console.log(Array.of({name : "hitesh"})) //interesting
+
+
+let score1 = 100
+let score2 = 200 
+let score3 = 300 
+
+console.group(Array.of(score1,score2,score3));
