1a976a3b1ef8c4191217c7d02f0cc0dfc7440511
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-08-17 22:44:21 +0530

    one

---
+++ Learning_javascript/javascriptbychaicode/control flow/one.js
@@ -0,0 +1,50 @@
+// if 
+const isUserloggedIn = true
+const temperature = 41
+
+// if(temperature == 41){
+//     console.log("less than 50")
+// }
+// else{
+//     console.log("temperature is greater than 50")
+// }
+
+// console.log("Execute")
+// <,>,>=,<=,!=, ==, ===, !==
+
+// const score = 200;
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User is ${power}`)
+// }
+
+const balance = 1000
+
+// if (balance > 500)  console.log("test"),console.log("test1")
+
+// if(balance < 500){
+//     console.log("less than 500");
+// }else if( balance < 750){
+//     console.log("less than 750");
+// }else if( balance < 900){
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("Other than given number")
+// }
+
+const userLoggedIn  = true;
+const debitcard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitcard){
+    console.log("user is logged in ")
+
+}
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("User has an ID")
+
+}
+
