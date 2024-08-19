241f1c86876d4ddbfff38b56aedf2c9562beda0f
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-08-19 22:48:47 +0530

    switchcase

---
+++ Learning_javascript/javascriptbychaicode/control flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "march"
+
+switch(month){
+    case "jan":
+        console.log("January")
+        break;
+    case "feb":
+        console.log("Febuary")
+        break;
+    case "mar":
+        console.log("March")
+        break;
+    default:
+        console.log("These are your month")
+}
+
+
