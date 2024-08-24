f5b92b367fb1b8bb6e5f25ff0397e9d1bef25ec1
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-08-24 23:07:40 +0530

    fcosn

---
+++ javaassignment/listreturn.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class listreturn {
+    public static void main(String[] args){
+    
+    }
+    static ArrayList<Integer> findAllIndex(int[] arr,int target,int index,ArrayList<Integer> list){
+
+        return list;
+    }
+}
+
