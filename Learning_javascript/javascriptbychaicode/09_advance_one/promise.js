4bf7c673388527aaad546c7f9c9dd9cf119ab15c
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-09-06 23:50:51 +0530

    df

---
+++ Learning_javascript/javascriptbychaicode/09_advance_one/promise.js
@@ -0,0 +1,52 @@
+const promiseOne = new Promise(function(resolve,reject){
+    // Do asyn task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log(('Asyn task is complete'));
+        resolve()
+    },1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Anonymous function runs")
+        resolve();
+    },1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username : "chai" , email:"chai@example.com"})
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+
+    setTimeout(function(){
+
+        let error = true;
+        if(!error){
+            resolve({username : "hitesh",Password : "12345"});
+        }
+        else{
+            reject('ERROR: Something went wrong');
+        }
+    },1000)
+})
+
+promiseFour.then(function(){
+    console.log("Is it running");
+})
+
