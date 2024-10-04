6952e9cbd2a656e454f01ebf97edfdb87ebc9964
Aryansahu28 <aryansahu.vibranium@gmail.com>
2024-10-04 22:21:36 +0530

    l

---
+++ javaassignment/Nknights.java
@@ -0,0 +1,14 @@
+public class Nknights {
+    public static void main(String[] args){
+
+    }
+
+    public static void Knight(boolean[][] board,int row,int col,int knights){
+        if(knights == 0){
+            display(board);
+            System.out.println();
+        }
+
+        for()
+    }
+}
