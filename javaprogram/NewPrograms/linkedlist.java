28d5ac5ab85075c63302d0bca092929dbb51338c
Aryansahu28 <aryansahu.vibranium@gmail.com>
2025-01-02 23:10:35 +0530

    linked list

--- javaprogram/NewPrograms/linkedlist.java
+++ javaprogram/NewPrograms/linkedlist.java
@@ -8,8 +8,13 @@ public class linkedlist {
         l.add("A");
         l.add("B");
         l.add("C");
+        l.add("T");
         l.addFirst("E");
-        l.removeLast();
+        // l.removeLast();
+        int a= l.size();
+        String d = l.get((int)a/2);
         System.out.println(l);
+        System.out.println(a);
+        System.out.println(d);
     }
 }
